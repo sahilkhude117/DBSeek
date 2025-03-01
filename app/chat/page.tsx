a205b018154d97061ec7732d057eda92be94f3d2
sahilkhude117 <sahilkhude11@gmail.com>
2025-03-02 02:03:44 +0530

    added chat page

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,23 +1,27 @@
-'use client'
+"use client";
 
 import { useState } from "react";
-import { motion , AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
+import {
+  generateChartConfig,
+  generateQuery,
+  runGenerateSQLQuery,
+} from "../actions"
 import { Config, Result } from "@/lib/types";
 import { Loader2 } from "lucide-react";
 import { toast } from "sonner";
-import { Header } from "@/components/header";
-import { Search } from "@/components/search";
+import { ProjectInfo } from "@/components/project-info";
+import { Results } from "@/components/results";
 import { SuggestedQueries } from "@/components/suggested-queries";
 import { QueryViewer } from "@/components/query-viewer";
-import { ProjectInfo } from "@/components/project-info";
-import { generateChartConfig, generateQuery, runGenerateSQLQuery } from "../actions";
-import { Results } from "@/components/results";
+import { Search } from "@/components/search";
+import { Header } from "@/components/header";
 
 export default function Page() {
   const [inputValue, setInputValue] = useState("");
   const [submitted, setSubmitted] = useState(false);
   const [results, setResults] = useState<Result[]>([]);
-  const [colums, setColumns] = useState<string[]>([]);
+  const [columns, setColumns] = useState<string[]>([]);
   const [activeQuery, setActiveQuery] = useState("");
   const [loading, setLoading] = useState(false);
   const [loadingStep, setLoadingStep] = useState(1);
@@ -25,21 +29,18 @@ export default function Page() {
 
   const handleSubmit = async (suggestion?: string) => {
     const question = suggestion ?? inputValue;
-
     if (inputValue.length === 0 && !suggestion) return;
     clearExistingData();
-
     if (question.trim()) {
       setSubmitted(true);
     }
     setLoading(true);
     setLoadingStep(1);
     setActiveQuery("");
-
     try {
       const query = await generateQuery(question);
       if (query === undefined) {
-        toast.error("An error occured. Please try again.");
+        toast.error("An error occurred. Please try again.");
         setLoading(false);
         return;
       }
@@ -50,20 +51,20 @@ export default function Page() {
       setResults(companies);
       setColumns(columns);
       setLoading(false);
-      const generation = await generateChartConfig(companies, question)
-      setChartConfig(generation.config)
+      const generation = await generateChartConfig(companies, question);
+      setChartConfig(generation.config);
     } catch (e) {
       toast.error("An error occurred. Please try again.");
       setLoading(false);
     }
   };
 
-  const handleSuggestionClick = async ( suggestion: string) => {
+  const handleSuggestionClick = async (suggestion: string) => {
     setInputValue(suggestion);
     try {
       await handleSubmit(suggestion);
-    } catch(e) {
-      toast.error("An error occured. Please try again.");
+    } catch (e) {
+      toast.error("An error occurred. Please try again.");
     }
   };
 
@@ -81,16 +82,16 @@ export default function Page() {
   };
 
   return (
-    <div className="bg-neutral-50 dark:bg-neutral-900 flex items-start justify-center p-0 sm-p-8">
-      <div className="w-full max-w-4xl min-h-dvh dm:min-h-0 flex flex-col">
+    <div className="bg-neutral-50 dark:bg-neutral-900 flex items-start justify-center p-0 sm:p-8">
+      <div className="w-full max-w-4xl min-h-dvh sm:min-h-0 flex flex-col ">
         <motion.div
           className="bg-card rounded-xl sm:border sm:border-border flex-grow flex flex-col"
-          initial={{ opacity: 0}}
-          animate={{ opacity: 1}}
-          transition={{ duration: 0.5, ease: "easeOut"}}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5, ease: "easeOut" }}
         >
           <div className="p-6 sm:p-8 flex flex-col flex-grow">
-            <Header handleClear={handleClear}/>
+            <Header handleClear={handleClear} />
             <Search
               handleClear={handleClear}
               handleSubmit={handleSubmit}
@@ -108,12 +109,12 @@ export default function Page() {
                     <SuggestedQueries
                       handleSuggestionClick={handleSuggestionClick}
                     />
-                  ): (
+                  ) : (
                     <motion.div
                       key="results"
-                      initial={{ opacity: 0}}
-                      animate={{ opacity: 1}}
-                      exit={{ opacity: 0}}
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                      exit={{ opacity: 0 }}
                       layout
                       className="sm:h-full min-h-[400px] flex flex-col"
                     >
@@ -124,25 +125,25 @@ export default function Page() {
                         />
                       )}
                       {loading ? (
-                        <div>
-                          <Loader2/>
-                            <p>
-                              {loadingStep == 1
-                                ? "Generating SQL query..."
-                                : "Running SQL query..."}
-                            </p>
+                        <div className="h-full absolute bg-background/50 w-full flex flex-col items-center justify-center space-y-4">
+                          <Loader2 className="h-12 w-12 animate-spin text-muted-foreground" />
+                          <p className="text-foreground">
+                            {loadingStep === 1
+                              ? "Generating SQL query..."
+                              : "Running SQL query..."}
+                          </p>
                         </div>
                       ) : results.length === 0 ? (
-                        <div>
-                          <p>
+                        <div className="flex-grow flex items-center justify-center">
+                          <p className="text-center text-muted-foreground">
                             No results found.
                           </p>
                         </div>
-                      ): (
+                      ) : (
                         <Results
-                            results={results}
-                            chartConfig={chartConfig}
-                            columns={colums}
+                          results={results}
+                          chartConfig={chartConfig}
+                          columns={columns}
                         />
                       )}
                     </motion.div>
@@ -151,9 +152,9 @@ export default function Page() {
               </div>
             </div>
           </div>
-          {/* <ProjectInfo/> */}
+          <ProjectInfo />
         </motion.div>
       </div>
     </div>
-  )
+  );
 }
