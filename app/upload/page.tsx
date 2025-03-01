99fb2ada8ee3947d161d481bc80e94032b5ec043
sahilkhude117 <sahilkhude11@gmail.com>
2025-03-02 02:08:02 +0530

    upload page

---
+++ app/upload/page.tsx
@@ -0,0 +1,12 @@
+import FileUploader from "@/components/FileUploader";
+
+export default function Home() {
+    return (
+        <div className="min-h-screen flex flex-col items-center justify-center p-4 space-y-4">
+            <h1 className="text-2xl font-bold text-center">
+                Upload a CSV File
+            </h1>
+            <FileUploader assetType="CSV_FILE" />
+        </div>
+    );
+}
