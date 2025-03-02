9ba6d42e33d011fb183307a147f04e02b0565b54
sahilkhude117 <sahilkhude11@gmail.com>
2025-03-02 09:39:09 +0530

    wrapped with auth provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { GeistSans } from "geist/font/sans";
 import { ThemeProvider } from "next-themes";
 import { TableProvider } from "@/contexts/TableContext";
 import Navbar from "@/components/navbar";
+import { AuthProvider } from "./providers";
 
 export const metadata = {
   metadataBase: new URL("http://localhost:3000"),
@@ -21,10 +22,12 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={`${GeistMono.className} ${GeistSans.className}`}>
       <TableProvider>
+        <AuthProvider>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
         <Navbar />
             {children}   
         </ThemeProvider>
+      </AuthProvider>
       </TableProvider>
       </body>
     </html>
